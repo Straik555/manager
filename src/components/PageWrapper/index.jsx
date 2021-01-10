3627ec68572f9865956d8232a5f090b79d6401e7
Straik555 <54748889+Straik555@users.noreply.github.com>
2021-01-10 17:02:08 +0200

    Update index.jsx

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -6,18 +6,21 @@ import {ACTIONS} from "../../context/providerContext";
 import AddPhone from "../AddPhone";
 import {useCustomStore} from "../../context/useStore";
 
-const PageWrapper = ({children}) => {
+const PageWrapper = ({children, match}) => {
     const {dispatch} = useCustomStore()
     return (
         <div className={'phone-list'}>
             <div className={'phone-list_menu'}>
                 <Menu />
-                <div className={'header'}>
-                    <div className={'search-block'}>
-                        <input type="text" placeholder='search' onChange={e => dispatch({type: ACTIONS.SEARCH_PHONE, payload: e.target.value})} />
+                {
+                    match &&
+                    <div className={'header'}>
+                        <div className={'search-block'}>
+                            <input type="text" placeholder='search' onChange={e => dispatch({type: ACTIONS.SEARCH_PHONE, payload: e.target.value})} />
+                        </div>
+                        <AddPhone />
                     </div>
-                    <AddPhone />
-                </div>
+                }
             </div>
             <div className={'phone-list_children'}>
                 {children}
